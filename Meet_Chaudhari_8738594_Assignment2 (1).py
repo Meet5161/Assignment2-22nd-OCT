f8e46761bf64ada773abeb1be3a6cdefa9bbbb68
Meet5161 <mchaudhari8594@conestogac.on.ca>
2021-10-22 05:47:48 -0400

    First Commmit for Assignment 2

---
+++ Meet_Chaudhari_8738594_Assignment2 (1).py
@@ -0,0 +1,33 @@
+print ("The main purpose to make this program is to get familier with Python language and I really liked this project as a part of my curriculum because I learnt plenty of things while making this little bit Software. \n ")
+
+print (" Welcome to AM's Shirt Cost Calculator \n")
+
+print (" \n These are the available Colors we have. ")
+
+print (" 1 == Black 4.3$ ")
+print (" 2 == Blue  2.8$ ")
+print (" 3 == white 8.5$ ")
+print (" 4 == Yellow 3$ ")
+print (" 5 == Orange 5.1$ ")
+
+print (" \n What type of colour do you want to order? ")
+
+num = int(input())
+
+if num == 1:
+    print (" You selected Black ")
+    VOT = 4.3
+
+elif num == 2: 
+    print (" You selected Blue ")  
+    VOT =2.8
+
+elif num == 3:
+    print (" You selected white ")   
+    VOT =8.5
+
+elif num == 4:
+    print (" You selected Yellow ")
+    VOT =3
+
+
